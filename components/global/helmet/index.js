fc7f6a4f184b1ab07bcc909238a89ab63dbfd00f
CodeXMakerCompany <codexmaker12@gmail.com>
2021-10-17 10:23:39 -0500

    fix: adding keys

--- components/global/helmet/index.js
+++ components/global/helmet/index.js
@@ -10,21 +10,21 @@ export const SEOHelmet = ({ props }) => {
         rel="icon"
         href="https://res.cloudinary.com/codexmaker/image/upload/v1630026497/logos/codexmaker_logo_my3ecl.png"
       />
-      <meta property="og:title" content={props.title} />
-      <meta property="og:description" content={props.description} />
-      <meta property="og:url" content={props.url} />
-      <meta property="og:type" content={props.og_type} />
-      <meta property="og:image" content={props.image} />
-      <meta property="og:image:width" content="250" />
-      <meta property="og:image:height" content="250" />
-      <meta property="og:image:alt" content={props.title} />
-      <meta property="og:site_name" content={props.title} />
-      <meta property="og:locale:alternate" content="es_MX" />
-      <meta property="twitter:card" content={props.tw_card} />
-      <meta property="twitter:url" content={props.url} />
-      <meta property="twitter:title" content={props.title} />
-      <meta property="twitter:description" content={props.description} />
-      <meta property="twitter:image" content={props.image} />
+      <meta property="og:title" content={props.title} key="title"/>
+      <meta property="og:description" content={props.description} key="description"/>
+      <meta property="og:url" content={props.url} key="url" />
+      <meta property="og:type" content={props.og_type} key="type" />
+      <meta property="og:image" content={props.image} key="image" />
+      <meta property="og:image:width" content="250" key="image:width"/>
+      <meta property="og:image:height" content="250" key="image:height"/>
+      <meta property="og:image:alt" content={props.title} key="image:alt"/>
+      <meta property="og:site_name" content={props.title} key="og:site_name"/>
+      <meta property="og:locale" content="es_MX" key="locale"/>
+      <meta property="twitter:card" content={props.tw_card} key="card"/>
+      <meta property="twitter:url" content={props.url} key="twitter:url"/>
+      <meta property="twitter:title" content={props.title} key="twitter:title"/>
+      <meta property="twitter:description" content={props.description} key="twitter:description"/>
+      <meta property="twitter:image" content={props.image} key="twitter:image"/>
     </Head>
   );
 };
