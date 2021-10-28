bc3dfddaf33a3069fb89b4d8b6f0d2ba34995917
CodeXMakerCompany <codexmaker12@gmail.com>
2021-10-27 21:32:16 -0500

    fix: if iframe exists

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   reactStrictMode: false,
   images: {
-    domains: ['res.cloudinary.com'],
+    domains: ["res.cloudinary.com"],
   },
-}
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.optimization.splitChunks.cacheGroups = {};
+    config.optimization.minimize = true;
+    return config;
+  },
+};
